5f7ae1c86c164018a0ecd62bc7fc9e2a70287268
Anders Thun <andersth@microsoft.com>
2018-12-18 13:52:09 +0100

    #1 Created project structure

---
+++ QueueFramework/QueueTestClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QueueTestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pq = new QueueLib.PriorityQueue();
+        }
+    }
+}
